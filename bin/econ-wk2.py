9af7d904a93836f8c3fd99ca7227e78f023c59f0
Ben W <dausha@gmail.com>
2019-03-23 08:17:44 -0400

    Preparing to switch to Master

--- bin/econ-wk2.py
+++ bin/econ-wk2.py
@@ -18,14 +18,17 @@ import statsmodels.api as sm
 from statsmodels.formula.api import ols
 
 data = pd.read_csv("data/econ-wk2-4.csv")
-x = data['Units']
-y = data['Price']
+y = data['Units']
+x = data['Price']
 
 ax = sns.regplot(x="Units", y="Price", data=data)
 ax.set_ylim(0,)
 ax.set_xlim(0,)
-beef_model = ols("Units ~ Price", data=data).fit()
-print(beef_model.summary())
+sm.add_constant(data)
+regression = ols("Units ~ Price", data=data).fit()
+print(regression.summary())
 
 fig = ax.get_figure()
 fig.savefig("images/econ/wk2-4.png")
+
+print(sy.stats.linregress(x,y))
